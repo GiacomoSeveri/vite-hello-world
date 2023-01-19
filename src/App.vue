d7181d943bcdf54b9bfe659d9bf5e1dc2cb25691
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-19 16:41:36 +0100

    finish with bonus

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,32 @@
+<script>
+// javascript
+import AppTitle from './components/Title.vue';
+import AppBanner from './components/Banner.vue';
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    AppTitle: AppTitle,
+    AppBanner: AppBanner
+  }
+}
+</script>
 
+<template>
+  <!-- html -->
+  <div>
+    <AppTitle></AppTitle>
+    <AppBanner></AppBanner>
+  </div>
+</template>
+
+<style>
+/* css */
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,28 @@
+<script>
+import { assert } from '@vue/compiler-core';
+
+export default {
+    name: 'AppBanner',
+    data() {
+        return {
+            image: 'BannerDBZ.jpeg'
+        }
+    },
+    methods: {
+        buildImg(image) {
+            const url = new URL(`../assets/${image}`, import.meta.url);
+            return url.href;
+        }
+    }
+}
+</script>
+
+<template>
+    <img :src="buildImg(image)" alt="se sei contrario sei un depensante">
+</template>
+
+<style scoped>
+img {
+    width: 850px;
+}
+</style>
